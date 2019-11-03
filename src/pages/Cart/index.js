044436b8d4fd123db3e48c964a08d866a56e0606
Everton <vertopge@hotmail.com>
2019-11-03 18:35:54 -0300

    chore: cart styled

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import color from '../../styles/colors';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+} from './styles';
 
 export default class Cart extends Component {
-  handleHome = () => {
-    const { navigation } = this.props;
-
-    navigation.navigate('Home');
-  };
+  componentDidMount() {}
 
   render() {
     return (
-      <View>
-        <Text>Cart</Text>
-        <Button title="Home" onPress={this.handleHome} />
-      </View>
+      <Container>
+        <Product>
+          <ProductInfo>
+            <ProductImage
+              source={{
+                uri:
+                  'https://static.netshoes.com.br/produtos/tenis-nike-shox-nz-masculino/12/D12-9660-112/D12-9660-112_detalhe2.jpg?ims=326x',
+              }}
+            />
+            <ProductDetails>
+              <ProductTitle>Tênis de Caminhada Leve Confortavel</ProductTitle>
+              <ProductPrice>R$179,90</ProductPrice>
+            </ProductDetails>
+            <ProductDelete>
+              <Icon name="delete-forever" size={24} color={color.primary} />
+            </ProductDelete>
+          </ProductInfo>
+
+          <ProductControls>
+            <ProductControlButton>
+              <Icon
+                name="remove-circle-outline"
+                size={20}
+                color={color.primary}
+              />
+            </ProductControlButton>
+
+            <ProductAmount />
+
+            <ProductControlButton>
+              <Icon name="add-circle-outline" size={20} color={color.primary} />
+            </ProductControlButton>
+
+            <ProductSubtotal>R$539,70</ProductSubtotal>
+          </ProductControls>
+        </Product>
+
+        <TotalContainer>
+          <TotalText>TOTAL</TotalText>
+          <TotalAmount>R$1690,10</TotalAmount>
+          <Order>
+            <OrderText>FINALIZAR PEDIDO</OrderText>
+          </Order>
+        </TotalContainer>
+      </Container>
     );
   }
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,106 @@
+import styled from 'styled-components/native';
+import color from '../../styles/colors';
+
+export const Container = styled.View`
+  min-height: 200px;
+  margin: 20px;
+  border-radius: 4px;
+  background: #fff;
+`;
+
+export const Product = styled.View`
+  padding: 15px;
+`;
+
+export const ProductInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductImage = styled.Image`
+  width: 80px;
+  height: 80px;
+`;
+
+export const ProductDetails = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProductTitle = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 14px;
+  color: #333;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 5px;
+`;
+
+export const ProductDelete = styled.TouchableOpacity`
+  padding: 5px;
+`;
+
+export const ProductControls = styled.View`
+  flex-direction: row;
+  align-items: center;
+  background: #eee;
+  padding: 8px;
+  border-radius: 4px;
+`;
+
+export const ProductControlButton = styled.TouchableOpacity``;
+
+export const ProductAmount = styled.TextInput.attrs({
+  editable: false,
+})`
+  background: #fff;
+  padding: 5px;
+  margin: 0px 5px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  min-width: 52px;
+  max-width: 120px;
+  height: 26px;
+`;
+
+export const ProductSubtotal = styled.Text`
+  flex: 1;
+  text-align: right;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const TotalContainer = styled.View`
+  margin: 10px;
+`;
+
+export const TotalText = styled.Text`
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  color: #999;
+`;
+
+export const TotalAmount = styled.Text`
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 30px;
+`;
+
+export const Order = styled.TouchableOpacity`
+  background: ${color.primary};
+  padding: 12px;
+  border-radius: 4px;
+`;
+
+export const OrderText = styled.Text`
+  text-align: center;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+`;
